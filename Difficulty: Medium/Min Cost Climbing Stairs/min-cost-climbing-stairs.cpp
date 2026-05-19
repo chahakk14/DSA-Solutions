52c86b98b53fc3366b2001f68b15fcb7be38f07a
Chahak Khandelwal <chahakkhandelwal.14@gmail.com>
2026-05-19 20:12:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Min Cost Climbing Stairs/min-cost-climbing-stairs.cpp
@@ -0,0 +1,15 @@
+//Back-end complete function Template for C++
+
+class Solution {
+  public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int prev2=cost[0];
+        int prev1= cost[1];
+        for(int i=2;i<cost.size();i++){
+            int curr= cost[i]+min(prev1,prev2);
+            prev2=prev1;
+            prev1=curr;
+        }
+        return min(prev1,prev2);
+    }
+};
